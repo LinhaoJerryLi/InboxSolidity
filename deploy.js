1d3378e48680a900657acccdf63585b81f5842ed
LinhaoJerryLi <mail@linhaoli.com>
2022-03-14 13:03:28 +0800

    update to 0.8.9

--- deploy.js
+++ deploy.js
@@ -1,10 +1,11 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
 
-const provider = new HDWalletProvider(
-  "MCode here",
-  "https://rinkeby.infura.io/v3/***"
+const { abi, evm } = require('./compile');
+
+provider = new HDWalletProvider(
+  'REPLACE_WITH_YOUR_MNEMONIC',
+  'REPLACE_WITH_YOUR_INFURA_URL'
 );
 
 const web3 = new Web3(provider);
@@ -12,13 +13,13 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Account: ", accounts[0]);
+  console.log('Attempting to deploy from account', accounts[0]);
 
-  const inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi There!"] })
-    .send({ from: accounts[0], gas: "1000000" });
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
 
-  console.log("Contract deployed to: ", inbox.options.address);
+  console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
 deploy();
